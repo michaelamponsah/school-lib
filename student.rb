998400b972569ebb42c4cbfc405565f4f0c327ef
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-03-10 20:25:04 +0000

    Modify student class to include classroom relationships

--- student.rb
+++ student.rb
@@ -4,7 +4,10 @@ class Student < Person
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
+    classroom.add_student(self)
   end
+
+  attr_accessor :classroom
 
   def play_hooky()
     '¯(ツ)/¯'
