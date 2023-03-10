2b3908e006b4c06a06889ff51e62eaaecde9dba1
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-03-10 20:23:35 +0000

    Implement Rental class

---
+++ rental.rb
@@ -0,0 +1,13 @@
+class Rental
+  def initialize(date, person, book)
+    @date = date
+
+    @person = person
+    @person.add_rental(self)
+
+    @book = book
+    book.add_rental(self)
+  end
+
+  attr_accessor :date
+end
