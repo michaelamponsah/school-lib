01d37080687e6131d2ffabc1a17913e73fb20b97
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-03-10 20:24:15 +0000

    Modify Person class to include rentals

--- person.rb
+++ person.rb
@@ -10,6 +10,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   attr_reader :id
@@ -21,6 +22,10 @@ class Person < Nameable
 
   def correct_name
     @name
+  end
+
+  def add_rental(rental)
+    @rentals.push(rental)
   end
 
   private
